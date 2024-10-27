e9c32cef6aab9f240ec5f2159a2d2a8c167059b8
tefutefu-nameko <kouta10071007@gmail.com>
2024-10-28 02:57:54 +0900

    シーン間のBGM保持
    仕様
    DontDestroyOnLoad使ってシーン間もオブジェクト情報を保持
    現在のシーンの名前を1fごとに確認してシーン名がゲーム画面の名前だったら直ちにBGMゲームオブジェクトを破壊するという力技でクリア
    なお、メニュー画面から始めた場合はBGM再生されません

---
+++ Assets/Scripts/KeepBGM.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.U2D.Animation;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class KeepBGM : MonoBehaviour
+{
+    public static KeepBGM instance;
+
+    void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Debug.LogWarning("EXTRA " + this + " DELETED");
+            Destroy(gameObject);
+        }
+    }
+
+
+    // Update is called once per frame
+    // ゲーム画面に移行したらメニュー画面のBGMオブジェクトを破壊する
+    void Update()
+    {
+        if (SceneManager.GetActiveScene().name.Equals("そふキャラヴァンサバ風ゲーム"))
+        {
+            Destroy(gameObject);
+        }
+    }
+
+
+    // ボタン操作のときはこの関数使う
+    public void DestroySingleton()
+    {
+        Destroy(gameObject);
+    }
+
+}
